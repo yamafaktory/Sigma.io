39c025a6c83f1ee8c55e668abb68d404641e497c
Davy Duperron <yamafaktory@gmail.com>
2013-12-28 11:00:26 +0100

    gulpfile update with dev server

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,38 @@ var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     myth = require('gulp-myth'),
     svgmin = require('gulp-svgmin'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    spawn = require('child_process').spawn,
+    nodeProcess,
+    nodeServer;
+
+nodeServer = {
+  kill : function () {
+    if (nodeProcess) {
+      nodeProcess.kill();
+      console.log('[\033[32mgulp\033[0m] \033[31mServer stopped\033[0m');
+    }
+  },
+  start : function () {
+    nodeProcess = spawn('node', ['server.js'], {stdio: 'inherit'});
+    console.log('[\033[32mgulp\033[0m] \033[33mServer started\033[0m');
+    nodeProcess.on('close', function (code) {
+      if (code === 8) {
+        console.log('Error detected, waiting for changes...');
+      }
+    });
+  },
+  restart : function () {
+    this.kill();
+    this.start();
+  },
+  onExitGulp : (function () {
+    var _this = this;
+    process.on('exit', function() {
+      _this.kill;
+    });
+  }())
+};
 
 gulp.task('test', function () {
   gulp.src('./src/js/*.js')
@@ -30,9 +61,13 @@ gulp.task('svg', function() {
     .pipe(gulp.dest('./public/img'));
 });
 
+gulp.task('server', function () {
+  nodeServer.restart();
+});
+
 gulp.task('default', function () {
-  gulp.run('test', 'js', 'css', 'svg');
+  gulp.run('test', 'js', 'css', 'svg', 'server');
   gulp.watch(['./src/js/*.js', './src/css/*.css', './src/svg/*.svg'], function () {
-    gulp.run('test', 'js', 'css', 'svg');
+    gulp.run('test', 'js', 'css', 'svg', 'server');
   });
 });
